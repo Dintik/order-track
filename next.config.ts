0f285aa4440cdaeab57f613f1346b5f8d4fcf3ca
Dintik <chailo.artem@gmail.com>
2025-05-01 02:41:15 +0300

    permission to load images from any domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**' // it is convenient to allow all hostnames as part of the test task
+      }
+    ]
+  }
 }
 
 export default nextConfig
